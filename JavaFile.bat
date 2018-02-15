6ff4007062771416214754329928bfc894649624
Rajkumar Surabhi <mailtorajuit@gmail.com>
2018-02-15 17:00:14 +0530

    files modified

--- JavaFile.bat
+++ JavaFile.bat
@@ -1,3 +1,3 @@
-java -cp "testng-6.8.3.jar;target/classes/;src/main/java/*;jcommander.jar;selenium-server-standalone-3.9.1.jar;" org.testng.TestNG .\testng.xml
+java -cp "jarfiles/testng-6.8.3.jar;target/classes/;src/main/java/*;jarfiles/jcommander.jar;jarfiles/selenium-server-standalone-3.9.1.jar;jarfiles/log4j-1.2.17.jar;" org.testng.TestNG .\testng.xml
 
 timeout 10
