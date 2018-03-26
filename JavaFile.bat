a271c7599d7ee3a0949f189374816eeea9a8e756
Rajkumar Surabhi <mailtorajuit@gmail.com>
2018-03-26 15:32:42 +0530

    script fixed

--- JavaFile.bat
+++ JavaFile.bat
@@ -1,3 +1,29 @@
-java -cp "jarfiles/testng-6.8.3.jar;target/classes/;src/main/java/*;jarfiles/jcommander.jar;jarfiles/selenium-server-standalone-3.9.1.jar;jarfiles/log4j-1.2.17.jar" org.testng.TestNG .\testng.xml
+@echo off
+SETLOCAL
+FOR /F "usebackq delims=" %%S IN (./src/main/resources/application.properties) DO (
+ echo %%S 
+ )
+FOR /F "eol=; tokens=2,2 delims==" %%i IN (./src/main/resources/application.properties) DO (
+ echo %%i
+ )
 
-timeout 10
+For /F "tokens=1* delims==" %%A IN (./src/main/resources/application.properties) DO (
+	echo %%A
+	IF "%%A"=="browser.url" set browser.url="test this"
+	IF "%%A"=="browser.test" set browser.test="test that" 
+	)
+	
+	
+(
+FOR /f "usebackqdelims=" %%a IN (./src/main/resources/application.properties) DO (
+FOR /f "tokens=1*delims==" %%g IN ("%%a") DO (
+IF "%%g"=="browser.name" (ECHO(%%g=%~1
+) ELSE (IF "%%g"=="browser.url" (ECHO(%%g=%~2
+) ELSE (ECHO(%%a)
+)
+)
+)
+)>temp1.properties
+
+COPY temp1.properties .\src\main\resources\application.properties
+del temp1.properties
